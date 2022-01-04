08e18609c842c3cefb388e78e61fe9e1d4cb6830
Andrew Premdas <apremdas@yahoo.co.uk>
2022-01-04 15:04:04 +0000

    Create a room
    Working scenario to create a room via the UI
    
    - Makandra standard controller
    - Model
    - Migration
    - Basic views

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,13 +3,46 @@ class RoomsController < ApplicationController
     load_rooms
   end
 
-  private
-  def load_rooms
-    @rooms = rooms_scope
+  def new
+    build_room
   end
 
-  def rooms_scope
-    Rooms.all
+  def show
+    load_room
+  end
+
+  def create
+    build_room
+    save_room or render 'new'
+  end
+
+  private
+  def load_rooms
+    @rooms = room_scope
+  end
+
+  def build_room
+    @room ||= room_scope.build
+    @room.attributes = room_params
+  end
+
+  def load_room
+    @room ||= room_scope.find(params[:id])
+  end
+
+  def save_room
+    if @room.save
+      redirect_to @room
+    end
+  end
+
+  def room_scope
+    Room.all
+  end
+
+  def room_params
+    room_params = params[:room]
+    room_params ? room_params.permit(:name) : {}
   end
 
 end
--- features/step_definitions/rooms_steps.rb
+++ features/step_definitions/rooms_steps.rb
@@ -2,13 +2,25 @@ module RoomsSH
 end
 World RoomsSH
 
+module FormSH
+  def submit_form
+    find('input[type="submit"]').click
+  end
+end
+World FormSH
+
 Given 'I am viewing the rooms' do
   visit root_path
 end
 
 When 'I create a room' do
-  binding.b
-  fill_in "room_name", with: Faker::Lorem.word
+  click_link "New"
+  fill_in "room_name", with: Faker::Name.name
+  submit_form
+end
+
+Then 'I should see the new room' do
+  expect(page).to have_css("#room_#{Room.last.id}")
 end
 
 
